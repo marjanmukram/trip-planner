2800e46c63db30b032080e5baee8d1a009eb993d
kiru07 <kirushanthan007@gmail.com>
2019-04-10 15:55:23 -0800

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import TripJoinPage from './components/join-trip/TripJoinPage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Trip planner</h1>
-        <TripJoinPage />
       </div>
     );
   }
