327d7b044b3eafef8f09784124fe4346936c5a1c
kiru07 <kirushanthan007@gmail.com>
2019-04-10 15:24:19 -0800

    Implemented Trip & TripList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Trip from './components/join-trip/Trip.jsx';
+import TripJoinPage from './components/join-trip/TripJoinPage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Trip planner</h1>
-        <Trip />
+        <TripJoinPage />
       </div>
     );
   }
--- src/components/join-trip/Trip.jsx
+++ src/components/join-trip/Trip.jsx
@@ -3,24 +3,45 @@ import React, { Component } from 'react';
 class Trip extends Component {
     constructor(props) {
         super(props);
+
     }
 
     render() {
+        // extract properties from props
+        const { name, planner, destinations, startDate, endDate, currentTravellers } = this.props.trip;
+
+        // create destination string
+        let destinationString = "";
+        for (let i = 0; i < destinations.length; i++) {
+            if (i === destinations.length - 1) {
+                destinationString += destinations[i];
+                break;
+            }
+            destinationString += destinations[i] + " to ";
+        }
+
+        // create img string
+        let imgSrc = "https://source.unsplash.com/1600x900/?" + destinations[0];
+
         return (
 
-            <div class="col s12 m7">
-                <h2 class="header">Horizontal Card</h2>
-                <div class="card horizontal">
-                    <div class="card-image">
-                        <img src="https://lorempixel.com/100/190/nature/6" />
+            <div className="col m12 m7">
+                <div className="card horizontal">
+                    <div className="card-image">
+                        <img src={imgSrc} />
                     </div>
-                    <div class="card-stacked">
-                        <div class="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    <div className="card-stacked">
+                        <div className="card-content">
+                            <h5>{name}</h5>
+                            <p>By: {planner}</p>
+                            <p>Destinations: {destinationString}</p>
+                            <p className="row"><span className="col m6">start: {new Date(startDate).toDateString()}</span><span className="col m6">end: {new Date(endDate).toDateString()}</span></p>
+                            <p>Current travellers: <span className="green-text lighten-2">{currentTravellers}</span></p>
                         </div>
-                        <div class="card-action">
-                            <a href="#">This is a link</a>
-                        </div>
+                        <button className="btn waves-effect waves-light" type="submit" name="action">Join Trip
+                                <i className="material-icons right">send</i>
+                        </button>
+
                     </div>
                 </div>
             </div>
