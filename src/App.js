ecf26ce7c1b94df909ac1c6e75000c9536afc49c
VithuJey <vithushjeytharma@gmail.com>
2019-04-10 18:01:14 +0530

    add detailed view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import TripDetails from './components/trip-details/tripDetails';
+import TripPlanForm from './components/trip-plan-form';
 
 class App extends Component {
   render() {
     return (
       <div>
 
-        <TripDetails />
+        <TripPlanForm />
 
       </div>
     );
