67776310328507b7965cfb7bd64be1e89c7546a6
kiru07 <kirushanthan007@gmail.com>
2019-04-10 13:27:15 -0800

    Created join-trip components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Trip from './components/join-trip/Trip.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Trip planner</h1>
+        <Trip />
       </div>
     );
   }
