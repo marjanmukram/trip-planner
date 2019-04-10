e0e9748f8c8593d11b78def6ee3458829586214b
Marjan <marjan.emeraldit@gmail.com>
2019-04-10 17:16:01 +0530

    Trip Details merged and bug fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import TripJoinPage from './components/join-trip/TripJoinPage';
-
+import TripDetails from './components/trip-details/tripDetails';
 import Nav from './components/Navigation';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import TripJoinPage from './components/join-trip/TripJoinPage';
 import TripPlanForm from './components/trip-plan-form';
 
 class App extends Component {
@@ -24,14 +23,14 @@ class App extends Component {
                   <Link to="/trip-plan-form/">Plan a trip</Link>
                 </li>
                 <li>
-                  <Link to="/joinTrip/">Join Trip</Link>
+                  <Link to="/trip-details/">Trip Details</Link>
                 </li>
               </ul>
             </div>
           </nav>
           <Route path="/joinTrip/" exact component={TripJoinPage} />
           <Route path="/trip-plan-form/" exact component={TripPlanForm} />
-          <Route path="/joinTrip/" exact component={TripJoinPage} />
+          <Route path="/trip-details/" exact component={TripDetails} />
         </div>
       </Router>
     );
